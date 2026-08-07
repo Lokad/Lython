bbc9ebe35a75714214dbe7f64508d12365e5b1e0
Joannes Vermorel <joannes.vermorel@lokad.com>
2026-08-07 17:57:25 +0200

    Analysis: Unify user call argument binding

--- src/Lokad.Lython/Frontend/StaticBindingEngine.Functions.cs
+++ src/Lokad.Lython/Frontend/StaticBindingEngine.Functions.cs
@@ -104,42 +104,63 @@ internal static partial class StaticBindingEngine
             return true;
         }
 
-        var effectiveParameters = parameters.Skip(firstParameterIndex).ToArray();
-        var positionalParameters = effectiveParameters.Where(static parameter => parameter.Kind == FunctionParameterKind.Positional).ToArray();
-        if (arguments.Positional.Count > positionalParameters.Length)
+        var effectiveParameterCount = parameters.Count - firstParameterIndex;
+        Span<bool> assigned = effectiveParameterCount <= 128
+            ? stackalloc bool[effectiveParameterCount]
+            : new bool[effectiveParameterCount];
+        Dictionary<string, int>? parameterIndices = arguments.Keywords.Count == 0
+            ? null
+            : new Dictionary<string, int>(effectiveParameterCount, StringComparer.Ordinal);
+
+        var positionalParameterCount = 0;
+        for (var parameterIndex = firstParameterIndex; parameterIndex < parameters.Count; parameterIndex++)
+        {
+            var effectiveIndex = parameterIndex - firstParameterIndex;
+            var parameter = parameters[parameterIndex];
+            parameterIndices?.Add(parameter.Name, effectiveIndex);
+            if (parameter.Kind != FunctionParameterKind.Positional)
+            {
+                continue;
+            }
+
+            if (positionalParameterCount < arguments.Positional.Count)
+            {
+                assigned[effectiveIndex] = true;
+            }
+
+            positionalParameterCount++;
+        }
+
+        if (arguments.Positional.Count > positionalParameterCount)
         {
             reason = "too many positional arguments";
             offendingExpression = null;
             return true;
         }
 
-        var assigned = new HashSet<string>(StringComparer.Ordinal);
-        for (var i = 0; i < arguments.Positional.Count; i++)
-        {
-            assigned.Add(positionalParameters[i].Name);
-        }
-
         foreach (var (keyword, expression) in arguments.Keywords)
         {
-            var parameter = effectiveParameters.FirstOrDefault(candidate => string.Equals(candidate.Name, keyword, StringComparison.Ordinal));
-            if (parameter is null)
+            if (parameterIndices is null || !parameterIndices.TryGetValue(keyword, out var effectiveIndex))
             {
                 reason = $"unexpected keyword '{keyword}'";
                 offendingExpression = expression;
                 return true;
             }
 
-            if (!assigned.Add(keyword))
+            if (assigned[effectiveIndex])
             {
                 reason = $"duplicate binding for '{keyword}'";
                 offendingExpression = expression;
                 return true;
             }
+
+            assigned[effectiveIndex] = true;
         }
 
-        foreach (var parameter in effectiveParameters)
+        for (var parameterIndex = firstParameterIndex; parameterIndex < parameters.Count; parameterIndex++)
         {
-            if (parameter.DefaultValue is null && !assigned.Contains(parameter.Name))
+            var parameter = parameters[parameterIndex];
+            if (parameter.DefaultValue is null && !assigned[parameterIndex - firstParameterIndex])
             {
                 reason = $"missing required argument '{parameter.Name}'";
                 offendingExpression = null;
@@ -157,77 +178,7 @@ internal static partial class StaticBindingEngine
         ConcreteCallArguments arguments,
         AbstractState callBindings,
         out AbstractState functionBindings)
-    {
-        functionBindings = summary.CapturedBindings.Clone();
-        var consumedKeywords = new HashSet<string>(StringComparer.Ordinal);
-        var positionalIndex = 0;
-
-        foreach (var parameter in summary.Parameters)
-        {
-            if (parameter.Kind == FunctionParameterKind.KeywordOnly)
-            {
-                if (arguments.Keywords.TryGetValue(parameter.Name, out var keywordExpression))
-                {
-                    consumedKeywords.Add(parameter.Name);
-                    functionBindings.Set(parameter.Name, arguments.ResolveKeywordValue(parameter.Name, callBindings));
-                }
-                else if (parameter.DefaultValue is not null)
-                {
-                    functionBindings.Set(parameter.Name, ResolveArgumentValue(parameter.DefaultValue, summary.CapturedBindings));
-                }
-                else
-                {
-                    return false;
-                }
-
-                continue;
-            }
-
-            if (parameter.Kind != FunctionParameterKind.Positional)
-            {
-                return false;
-            }
-
-            if (positionalIndex < arguments.Positional.Count)
-            {
-                if (arguments.Keywords.ContainsKey(parameter.Name))
-                {
-                    return false;
-                }
-
-                functionBindings.Set(parameter.Name, arguments.ResolvePositionalValue(positionalIndex, callBindings));
-                positionalIndex++;
-            }
-            else if (arguments.Keywords.TryGetValue(parameter.Name, out var keywordExpression))
-            {
-                consumedKeywords.Add(parameter.Name);
-                functionBindings.Set(parameter.Name, arguments.ResolveKeywordValue(parameter.Name, callBindings));
-            }
-            else if (parameter.DefaultValue is not null)
-            {
-                functionBindings.Set(parameter.Name, ResolveArgumentValue(parameter.DefaultValue, summary.CapturedBindings));
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        if (positionalIndex != arguments.Positional.Count)
-        {
-            return false;
-        }
-
-        foreach (var keyword in arguments.Keywords.Keys)
-        {
-            if (!consumedKeywords.Contains(keyword))
-            {
-                return false;
-            }
-        }
-
-        return true;
-    }
+        => TryBindParameterArguments(summary, firstParameterIndex: 0, receiver: null, arguments, callBindings, out functionBindings);
 
     private static bool TryBindInstanceMethodArguments(
         AbstractFunctionSummary summary,
@@ -235,30 +186,50 @@ internal static partial class StaticBindingEngine
         ConcreteCallArguments arguments,
         AbstractState callBindings,
         out AbstractState methodBindings)
+        => TryBindParameterArguments(summary, firstParameterIndex: 1, selfValue, arguments, callBindings, out methodBindings);
+
+    private static bool TryBindParameterArguments(
+        AbstractFunctionSummary summary,
+        int firstParameterIndex,
+        AbstractValue? receiver,
+        ConcreteCallArguments arguments,
+        AbstractState callBindings,
+        out AbstractState parameterBindings)
     {
-        methodBindings = summary.CapturedBindings.Clone();
-        if (summary.Parameters.Count == 0 || summary.Parameters[0].Kind != FunctionParameterKind.Positional)
+        parameterBindings = summary.CapturedBindings.Clone();
+        if (firstParameterIndex > summary.Parameters.Count)
         {
             return false;
         }
 
-        methodBindings.Set(summary.Parameters[0].Name, selfValue.WithSpan(summary.Span));
-        var consumedKeywords = new HashSet<string>(StringComparer.Ordinal);
+        if (receiver.HasValue)
+        {
+            if (firstParameterIndex != 1 ||
+                summary.Parameters.Count == 0 ||
+                summary.Parameters[0].Kind != FunctionParameterKind.Positional)
+            {
+                return false;
+            }
+
+            parameterBindings.Set(summary.Parameters[0].Name, receiver.Value.WithSpan(summary.Span));
+        }
+
+        var consumedKeywordCount = 0;
         var positionalIndex = 0;
 
-        for (var parameterIndex = 1; parameterIndex < summary.Parameters.Count; parameterIndex++)
+        for (var parameterIndex = firstParameterIndex; parameterIndex < summary.Parameters.Count; parameterIndex++)
         {
             var parameter = summary.Parameters[parameterIndex];
             if (parameter.Kind == FunctionParameterKind.KeywordOnly)
             {
-                if (arguments.Keywords.TryGetValue(parameter.Name, out var keywordExpression))
+                if (arguments.Keywords.ContainsKey(parameter.Name))
                 {
-                    consumedKeywords.Add(parameter.Name);
-                    methodBindings.Set(parameter.Name, arguments.ResolveKeywordValue(parameter.Name, callBindings));
+                    consumedKeywordCount++;
+                    parameterBindings.Set(parameter.Name, arguments.ResolveKeywordValue(parameter.Name, callBindings));
                 }
                 else if (parameter.DefaultValue is not null)
                 {
-                    methodBindings.Set(parameter.Name, ResolveArgumentValue(parameter.DefaultValue, summary.CapturedBindings));
+                    parameterBindings.Set(parameter.Name, ResolveArgumentValue(parameter.DefaultValue, summary.CapturedBindings));
                 }
                 else
                 {
@@ -280,17 +251,17 @@ internal static partial class StaticBindingEngine
                     return false;
                 }
 
-                methodBindings.Set(parameter.Name, arguments.ResolvePositionalValue(positionalIndex, callBindings));
+                parameterBindings.Set(parameter.Name, arguments.ResolvePositionalValue(positionalIndex, callBindings));
                 positionalIndex++;
             }
-            else if (arguments.Keywords.TryGetValue(parameter.Name, out var keywordExpression))
+            else if (arguments.Keywords.ContainsKey(parameter.Name))
             {
-                consumedKeywords.Add(parameter.Name);
-                methodBindings.Set(parameter.Name, arguments.ResolveKeywordValue(parameter.Name, callBindings));
+                consumedKeywordCount++;
+                parameterBindings.Set(parameter.Name, arguments.ResolveKeywordValue(parameter.Name, callBindings));
             }
             else if (parameter.DefaultValue is not null)
             {
-                methodBindings.Set(parameter.Name, ResolveArgumentValue(parameter.DefaultValue, summary.CapturedBindings));
+                parameterBindings.Set(parameter.Name, ResolveArgumentValue(parameter.DefaultValue, summary.CapturedBindings));
             }
             else
             {
@@ -298,20 +269,7 @@ internal static partial class StaticBindingEngine
             }
         }
 
-        if (positionalIndex != arguments.Positional.Count)
-        {
-            return false;
-        }
-
-        foreach (var keyword in arguments.Keywords.Keys)
-        {
-            if (!consumedKeywords.Contains(keyword))
-            {
-                return false;
-            }
-        }
-
-        return true;
+        return positionalIndex == arguments.Positional.Count && consumedKeywordCount == arguments.Keywords.Count;
     }
 
     private static AbstractValue ResolveArgumentValue(ExpressionSyntax expression, AbstractState bindings)
