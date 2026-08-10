0d5e591067e3b3921ad725f5d980f5ad962e6447
Joannes Vermorel <joannes.vermorel@lokad.com>
2026-08-10 18:52:40 +0200

    Runtime: Share print argument binding

--- src/Lokad.Lython/Runtime/LythonRuntime.Calls.cs
+++ src/Lokad.Lython/Runtime/LythonRuntime.Calls.cs
@@ -566,104 +566,28 @@ internal sealed partial class LythonRuntime
         public object Invoke(CallArgumentValue[] arguments, LythonSourceSpan span, ExecutionContext context)
         {
             context.CheckExecutionBudget(span);
-
-            var positional = new List<object>(arguments.Length);
-            PyString separator = DefaultPrintSeparator;
-            PyString ending = DefaultPrintEnding;
-            object? outputTarget = null;
-            var flush = false;
-            var seenSeparator = false;
-            var seenEnding = false;
-            var seenFile = false;
-            var seenFlush = false;
-
+            var bound = BindArguments(arguments, span);
+            var hasWrittenValue = false;
             foreach (var argument in arguments)
             {
-                if (argument.IsPositional)
+                if (!argument.IsPositional)
                 {
-                    positional.Add(argument.Value);
                     continue;
                 }
 
-                switch (argument.KeywordName)
+                if (hasWrittenValue)
                 {
-                    case "sep":
-                        if (seenSeparator)
-                        {
-                            throw CallErrors.MultipleValues(PythonCallableKind.Builtin, "print", "sep", span);
-                        }
-
-                        separator = ReferenceEquals(argument.Value, PyNone.Instance)
-                            ? DefaultPrintSeparator
-                            : PyStringOps.TryAsString(argument.Value, out var separatorValue)
-                                ? separatorValue
-                                : throw new LythonRuntimeException("TypeError", "print(..., sep=...) expects a string or None.", span);
-                        seenSeparator = true;
-                        break;
-
-                    case "end":
-                        if (seenEnding)
-                        {
-                            throw CallErrors.MultipleValues(PythonCallableKind.Builtin, "print", "end", span);
-                        }
-
-                        ending = ReferenceEquals(argument.Value, PyNone.Instance)
-                            ? DefaultPrintEnding
-                            : PyStringOps.TryAsString(argument.Value, out var endingValue)
-                                ? endingValue
-                                : throw new LythonRuntimeException("TypeError", "print(..., end=...) expects a string or None.", span);
-                        seenEnding = true;
-                        break;
-
-                    case "file":
-                        if (seenFile)
-                        {
-                            throw CallErrors.MultipleValues(PythonCallableKind.Builtin, "print", "file", span);
-                        }
-
-                        outputTarget = argument.Value switch
-                        {
-                            PyNone => null,
-                            ExecutionContext.TextFileHandle handle => handle,
-                            HostTextOutputHandle handle => handle,
-                            PopenInputStream handle => handle,
-                            _ => throw new LythonRuntimeException(
-                                "TypeError",
-                                "print(..., file=...) expects a writable Lython text stream, writable file handle, or None.",
-                                span)
-                        };
-                        seenFile = true;
-                        break;
-
-                    case "flush":
-                        if (seenFlush)
-                        {
-                            throw CallErrors.MultipleValues(PythonCallableKind.Builtin, "print", "flush", span);
-                        }
-
-                        flush = IsTruthy(argument.Value);
-                        seenFlush = true;
-                        break;
-
-                    default:
-                        throw CallErrors.UnexpectedKeyword(PythonCallableKind.Builtin, "print", argument.KeywordName, span);
-                }
-            }
-
-            for (var i = 0; i < positional.Count; i++)
-            {
-                if (i > 0)
-                {
-                    AppendOutput(separator, context, outputTarget, span);
+                    AppendOutput(bound.Separator, context, bound.OutputTarget, span);
                 }
 
-                AppendOutput(ToInterpolatedPyString(positional[i], context), context, outputTarget, span);
+                AppendOutput(ToInterpolatedPyString(argument.Value, context), context, bound.OutputTarget, span);
+                hasWrittenValue = true;
             }
 
-            AppendOutput(ending, context, outputTarget, span);
-            if (flush)
+            AppendOutput(bound.Ending, context, bound.OutputTarget, span);
+            if (bound.Flush)
             {
-                FlushOutput(context, outputTarget, span);
+                FlushOutput(context, bound.OutputTarget, span);
             }
 
             return PyNone.Instance;
@@ -672,11 +596,38 @@ internal sealed partial class LythonRuntime
         public async ValueTask<object> InvokeAsync(CallArgumentValue[] arguments, LythonSourceSpan span, ExecutionContext context)
         {
             context.CheckExecutionBudget(span);
+            var bound = BindArguments(arguments, span);
+            var hasWrittenValue = false;
+            foreach (var argument in arguments)
+            {
+                if (!argument.IsPositional)
+                {
+                    continue;
+                }
 
-            var positional = new List<object>(arguments.Length);
-            PyString separator = DefaultPrintSeparator;
-            PyString ending = DefaultPrintEnding;
-            object? outputTarget = null;
+                if (hasWrittenValue)
+                {
+                    await AppendOutputAsync(bound.Separator, context, bound.OutputTarget, span).ConfigureAwait(false);
+                }
+
+                await AppendOutputAsync(ToInterpolatedPyString(argument.Value, context), context, bound.OutputTarget, span).ConfigureAwait(false);
+                hasWrittenValue = true;
+            }
+
+            await AppendOutputAsync(bound.Ending, context, bound.OutputTarget, span).ConfigureAwait(false);
+            if (bound.Flush)
+            {
+                await FlushOutputAsync(context, bound.OutputTarget, span).ConfigureAwait(false);
+            }
+
+            return PyNone.Instance;
+        }
+
+        private static BoundPrintArguments BindArguments(CallArgumentValue[] arguments, LythonSourceSpan span)
+        {
+            var separator = DefaultPrintSeparator;
+            var ending = DefaultPrintEnding;
+            PrintOutputTarget outputTarget = StandardOutputTarget.Instance;
             var flush = false;
             var seenSeparator = false;
             var seenEnding = false;
@@ -687,7 +638,6 @@ internal sealed partial class LythonRuntime
             {
                 if (argument.IsPositional)
                 {
-                    positional.Add(argument.Value);
                     continue;
                 }
 
@@ -729,10 +679,10 @@ internal sealed partial class LythonRuntime
 
                         outputTarget = argument.Value switch
                         {
-                            PyNone => null,
-                            ExecutionContext.TextFileHandle handle => handle,
-                            HostTextOutputHandle handle => handle,
-                            PopenInputStream handle => handle,
+                            PyNone => StandardOutputTarget.Instance,
+                            ExecutionContext.TextFileHandle handle => new TextFileOutputTarget(handle),
+                            HostTextOutputHandle handle => new HostOutputTarget(handle),
+                            PopenInputStream handle => new PopenInputOutputTarget(handle),
                             _ => throw new LythonRuntimeException(
                                 "TypeError",
                                 "print(..., file=...) expects a writable Lython text stream, writable file handle, or None.",
@@ -756,108 +706,114 @@ internal sealed partial class LythonRuntime
                 }
             }
 
-            for (var i = 0; i < positional.Count; i++)
-            {
-                if (i > 0)
-                {
-                    await AppendOutputAsync(separator, context, outputTarget, span).ConfigureAwait(false);
-                }
-
-                await AppendOutputAsync(ToInterpolatedPyString(positional[i], context), context, outputTarget, span).ConfigureAwait(false);
-            }
-
-            await AppendOutputAsync(ending, context, outputTarget, span).ConfigureAwait(false);
-            if (flush)
-            {
-                await FlushOutputAsync(context, outputTarget, span).ConfigureAwait(false);
-            }
-
-            return PyNone.Instance;
+            return new BoundPrintArguments(separator, ending, outputTarget, flush);
         }
 
-        private static void AppendOutput(PyString value, ExecutionContext context, object? outputTarget, LythonSourceSpan span)
+        private static void AppendOutput(PyString value, ExecutionContext context, PrintOutputTarget outputTarget, LythonSourceSpan span)
         {
-            if (outputTarget is null)
+            switch (outputTarget)
             {
-                _ = context.State.Stdout.Write(value, span);
-            }
-
-            else if (outputTarget is ExecutionContext.TextFileHandle fileHandle)
-            {
-                _ = fileHandle.Write(value);
-            }
-
-            else if (outputTarget is HostTextOutputHandle outputHandle)
-            {
-                _ = outputHandle.Write(value, span);
-            }
-            else if (outputTarget is PopenInputStream popenInput)
-            {
-                _ = popenInput.Write(value, span);
+                case StandardOutputTarget:
+                    _ = context.State.Stdout.Write(value, span);
+                    break;
+                case TextFileOutputTarget file:
+                    _ = file.Handle.Write(value);
+                    break;
+                case HostOutputTarget host:
+                    _ = host.Handle.Write(value, span);
+                    break;
+                case PopenInputOutputTarget process:
+                    _ = process.Handle.Write(value, span);
+                    break;
+                default:
+                    throw new InvalidOperationException("Unsupported print output target.");
             }
         }
 
-        private static void FlushOutput(ExecutionContext context, object? outputTarget, LythonSourceSpan span)
+        private static void FlushOutput(ExecutionContext context, PrintOutputTarget outputTarget, LythonSourceSpan span)
         {
-            if (outputTarget is null)
+            switch (outputTarget)
             {
-                _ = context.State.Stdout.Flush(span);
-            }
-            else if (outputTarget is ExecutionContext.TextFileHandle fileHandle)
-            {
-                _ = fileHandle.Flush();
-            }
-            else if (outputTarget is HostTextOutputHandle outputHandle)
-            {
-                _ = outputHandle.Flush(span);
-            }
-            else if (outputTarget is PopenInputStream popenInput)
-            {
-                popenInput.FlushValue(span);
+                case StandardOutputTarget:
+                    _ = context.State.Stdout.Flush(span);
+                    break;
+                case TextFileOutputTarget file:
+                    _ = file.Handle.Flush();
+                    break;
+                case HostOutputTarget host:
+                    _ = host.Handle.Flush(span);
+                    break;
+                case PopenInputOutputTarget process:
+                    process.Handle.FlushValue(span);
+                    break;
+                default:
+                    throw new InvalidOperationException("Unsupported print output target.");
             }
         }
 
-        private static async ValueTask AppendOutputAsync(PyString value, ExecutionContext context, object? outputTarget, LythonSourceSpan span)
+        private static async ValueTask AppendOutputAsync(PyString value, ExecutionContext context, PrintOutputTarget outputTarget, LythonSourceSpan span)
         {
-            if (outputTarget is null)
+            switch (outputTarget)
             {
-                _ = await context.State.Stdout.WriteAsync(value, span).ConfigureAwait(false);
-            }
-
-            else if (outputTarget is ExecutionContext.TextFileHandle fileHandle)
-            {
-                _ = fileHandle.Write(value);
-            }
-
-            else if (outputTarget is HostTextOutputHandle outputHandle)
-            {
-                _ = await outputHandle.WriteAsync(value, span).ConfigureAwait(false);
-            }
-            else if (outputTarget is PopenInputStream popenInput)
-            {
-                _ = popenInput.Write(value, span);
+                case StandardOutputTarget:
+                    _ = await context.State.Stdout.WriteAsync(value, span).ConfigureAwait(false);
+                    break;
+                case TextFileOutputTarget file:
+                    _ = file.Handle.Write(value);
+                    break;
+                case HostOutputTarget host:
+                    _ = await host.Handle.WriteAsync(value, span).ConfigureAwait(false);
+                    break;
+                case PopenInputOutputTarget process:
+                    _ = process.Handle.Write(value, span);
+                    break;
+                default:
+                    throw new InvalidOperationException("Unsupported print output target.");
             }
         }
 
-        private static async ValueTask FlushOutputAsync(ExecutionContext context, object? outputTarget, LythonSourceSpan span)
+        private static async ValueTask FlushOutputAsync(ExecutionContext context, PrintOutputTarget outputTarget, LythonSourceSpan span)
         {
-            if (outputTarget is null)
+            switch (outputTarget)
             {
-                _ = await context.State.Stdout.FlushAsync(span).ConfigureAwait(false);
-            }
-            else if (outputTarget is ExecutionContext.TextFileHandle fileHandle)
-            {
-                _ = await fileHandle.FlushAsync().ConfigureAwait(false);
-            }
-            else if (outputTarget is HostTextOutputHandle outputHandle)
-            {
-                _ = await outputHandle.FlushAsync(span).ConfigureAwait(false);
-            }
-            else if (outputTarget is PopenInputStream popenInput)
-            {
-                popenInput.FlushValue(span);
+                case StandardOutputTarget:
+                    _ = await context.State.Stdout.FlushAsync(span).ConfigureAwait(false);
+                    break;
+                case TextFileOutputTarget file:
+                    _ = await file.Handle.FlushAsync().ConfigureAwait(false);
+                    break;
+                case HostOutputTarget host:
+                    _ = await host.Handle.FlushAsync(span).ConfigureAwait(false);
+                    break;
+                case PopenInputOutputTarget process:
+                    process.Handle.FlushValue(span);
+                    break;
+                default:
+                    throw new InvalidOperationException("Unsupported print output target.");
             }
         }
+
+        private readonly record struct BoundPrintArguments(
+            PyString Separator,
+            PyString Ending,
+            PrintOutputTarget OutputTarget,
+            bool Flush);
+
+        private abstract record PrintOutputTarget
+        {
+            private protected PrintOutputTarget() { }
+        }
+
+        private sealed record StandardOutputTarget : PrintOutputTarget
+        {
+            public static StandardOutputTarget Instance { get; } = new();
+        }
+
+        private sealed record TextFileOutputTarget(ExecutionContext.TextFileHandle Handle) : PrintOutputTarget;
+
+        private sealed record HostOutputTarget(HostTextOutputHandle Handle) : PrintOutputTarget;
+
+        private sealed record PopenInputOutputTarget(PopenInputStream Handle) : PrintOutputTarget;
     }
 
 }
